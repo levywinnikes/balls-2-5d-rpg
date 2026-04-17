b9ff5c55a9cbdc329d43695c6e48998a1cef3ac8
levywinnikes <levy.winnikes@gmail.com>
2026-04-17 16:52:05 -0300

    chore: remove unused MovementSystem placeholder

--- src/game/systems/MovementSystem.ts
+++
@@ -1,7 +0,0 @@
-import Phaser from "phaser";
-
-export default class MovementSystem {
-  // Implementação futura para sistema de movimento
-}
-
-export {}; // Para resolver o erro de isolatedModules
